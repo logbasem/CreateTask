36f9ee5c37c3fed28495b461147047408d12a180
logbasem <loganmichellemurphy@gmail.com>
2022-03-30 13:11:26 -0500

    Changeable buttons
    Added code to set the text of the buttons

--- adventure.js
+++ adventure.js
@@ -11,6 +11,18 @@ var textElement = document.getElementById("text");
 var optionButtonsElement = document.getElementById("option-buttons");
 
 textElement.innerText = "This is a test!";
+removeButtons();
+setButtons(["Test", "Test2", "Test3"]);
+
+function setButtons(optionArray) {
+    for (let i = 0; i < optionArray.length; i++) {
+        var button = document.createElement("button");
+        button.innerText = optionArray[i];
+        button.classList.add("btn");
+        optionButtonsElement.appendChild(button);
+    }
+}
+
 
 function removeButtons() {
     while (optionButtonsElement.firstChild) {
